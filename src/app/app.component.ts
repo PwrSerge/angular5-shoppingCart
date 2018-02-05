ce45659cee593465d0520082274f67fb07f64384
Serge Guilmette <sergeguilmette@gmail.com>
2018-02-05 14:36:10 -0500

    firebase data store and authentication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Recipe App';
   loadedFeature = 'recipe';
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDRHt3wGrZqRIZQbpeViLquWqdz3FwM_vk',
+      authDomain: 'recipe-app-f6848.firebaseapp.com'
+    });
+  }
+
   onNavigate(feature) {
     this.loadedFeature = feature;
   }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core/';
+import { DataStorageService } from '../common/data-storage.service';
+import { Response } from '@angular/http/';
 
 @Component({
     selector: 'app-header',
@@ -7,12 +9,27 @@ import { Component } from '@angular/core/';
 export class HeaderComponent {
     showMenu = false;
 
+    constructor(private dataStorageService: DataStorageService) {}
+
     setDropdownMenu() {
         if (this.showMenu) {
             return 'show';
         } else {
             return '';
         }
+    }
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes()
+        .subscribe(
+            (response: Response) => {
+                console.log(response);
+            }
+        );
+    }
+
+    onFetchData() {
+        this.dataStorageService.getRecipes();
     }
 
 }
